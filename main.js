0eba2470ec57d1457440cfda4024127da6f10b98
Gavin Szczesniak <gavinszczesniak@gmail.com>
2023-11-03 15:09:45 -0400

    Pool setup   - Installed package of pg to run pool to get my local DB to work on this repo   - Sets up the pool and tests it within a get request

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
+const Pool = require('pg').Pool;
 const express = require("express");
+
+const pool = new Pool({
+  host: 'localhost',
+  database: 'demo',
+  port: 5432,
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000
+});
 
 const app = express();
 const port = 3400;
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get('/', async (req, res) => {
+  await pool.query('SELECT * FROM aircrafts', (error, results) => {
+    try {
+      res.status(200).json(results.rows);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  });
 });
 
 app.listen(port, () => {
